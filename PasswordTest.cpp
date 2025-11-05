60ac2dc8722271adbaf24a39ba5939760446a501
Cameron Sweet <cameronbsweet@gmail.com>
2025-11-05 11:46:19 -0800

    added new test cases

--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -17,6 +17,25 @@ class PasswordTest : public ::testing::Test
 TEST(PasswordTest, single_letter_password)
 {
 	Password my_password;
-	int actual = my_password.count_leading_characters("Z");
-	ASSERT_EQ(1, actual);
+	ASSERT_EQ(1, my_password.count_leading_characters("Z"));
+}
+TEST(PasswordTest, double_leading_letters)
+{
+	Password my_password;
+	ASSERT_EQ(2, my_password.count_leading_characters("zz"));
+}
+TEST(PasswordTest, empty_password)
+{
+	Password my_password;
+	ASSERT_EQ(0, my_password.count_leading_characters(""));
+}
+TEST(PasswordTest, lowercase_uppercase_characters)
+{
+	Password my_password;
+	ASSERT_EQ(1, my_password.count_leading_characters("Zz"));
+}
+TEST(PasswordTest, space_character)
+{
+	Password my_password;
+	ASSERT_EQ(2, my_password.count_leading_characters("  "));
 }
